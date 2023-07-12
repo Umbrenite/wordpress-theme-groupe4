58ce2371aafb3c12c087f501f4b71c0f339e9e43
Umbrenite <blandin.arthur20@gmail.com>
2023-07-12 14:13:09 +0000

    :sparkles: first commit :sparkles:

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer id="site-footer" class="col-md-6 offset-3">
+			<div class="container">
+				©2023 ESGI - Groupe 4
+			</div>
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header() ?>
+
+<main id="site-content">
+        <div class="container">
+            <h1>ESGI - GROUPE 4</h1>
+        </div>
+</main>
+
+<?php get_footer() ?>
